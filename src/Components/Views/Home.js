6305ae7058dc4fab9f671e3ffb99edbfcdd906a6
kaaboaye <kaaboaye@gmail.com>
2018-02-25 00:17:39 +0100

    added map

--- src/Components/Views/Home.js
+++ src/Components/Views/Home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 
-import Map from '../../Images/TMP-bingmap.png'
 
 class Home extends Component {
+  frameLoad = () => {
+    const frame = this.refs.mapFrame
+
+    frame.height = frame.contentWindow.document.body.scrollHeight
+    // frame.contentWindow.eval("alert(window.location)")
+  }
+
   render() {
     return (
-      <div>
-        <img src={Map} alt="Map" width="100%" />
-      </div>
+      <iframe width="100%" ref="mapFrame" onLoad={this.frameLoad} src="/map.html" title="Map" scrolling="no"/>
     )
   }
 }
